385e21d56b205106461cccd6ec9c1f5fb8ed60fc
YanuarRamadhaniKuswoko <ryanuar867@gmail.com>
2025-04-15 09:34:44 +0700

    25-Barang2

--- Barang.java
+++ Barang.java
@@ -1,19 +1,23 @@
-public class Barang{
+public class Barang {
     String kodeBarang;
     String nama;
+    String kategori;
     int stok;
     int harga;
 
-    public Barang(String kodeBarang, String nama, int stok, int harga) {
+    public Barang(String kodeBarang, String nama, String kategori, int stok, int harga) {
         this.kodeBarang = kodeBarang;
-        this .nama = nama;
-        this .stok = stok;
-        this .harga = harga;
-}
-    public void tampilBarang(){
-        System.out.println("Kode Barang : "+kodeBarang);
-        System .out.println("Nama Barang : "+nama);
-        System .out.println("Kategori Barang : "+kategori);
-        System .out.println("Stok Barang : "+Stok);
-        System .out.println("Harga Barang : "+harga);
+        this.nama = nama;
+        this.kategori = kategori;
+        this.stok = stok;
+        this.harga = harga;
+    }
+
+    public void tampilBarang() {
+        System.out.println("Kode Barang      : " + kodeBarang);
+        System.out.println("Nama Barang      : " + nama);
+        System.out.println("Kategori Barang  : " + kategori);
+        System.out.println("Stok Barang      : " + stok);
+        System.out.println("Harga Barang     : Rp" + harga);
+    }
 }
